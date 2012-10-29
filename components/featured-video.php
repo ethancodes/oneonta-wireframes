0ec56eb36bf9f04cb21118bade938d5fbe13aca1
ethancodes <e@ethancodes.com>
2012-10-29 15:41:05 -0400

    finished admissions page
    this project is a complete mess now. hopefully i can fix it up, but
    since this is due in 48 hours i doubt it.

---
+++ components/featured-video.php
@@ -0,0 +1,20 @@
+<div class="featured-video graybox">
+	<h3>Featured Video</h3>
+	<div class="video">
+		<img src="images/video.jpg" alt="video" />
+	</div>
+	
+	<?php if (get_title() == 'admissions'): ?>
+	<div class="admissions-fv-text">While you're at it, check these out.</div>
+	<div class="admissions-fv-thumbs clearfix">
+		<div class="admissions-fv-thumb first"></div>
+		<div class="admissions-fv-thumb"></div>
+		<div class="admissions-fv-thumb last"></div>		
+	</div>
+	<div class="admissions-fv-icon-wrapper clearfix">
+		<div class="admissions-fv-icon"><img src="images/connect-youtube.jpg" /></div>
+		<div class="admissions-fv-icon-text">720 transition helipop rocket air Japan air skate or die.</div>
+	</div>
+	<?php endif; ?>
+	
+</div>
--- script.js
+++ script.js
@@ -31,24 +31,56 @@ function viewportGo() {
 	
 	if (thisviewport == "s" || thisviewport == "m") {
 	
-		$('.slideshow').html('<img src="http://placekitten.com/g/825/508" alt="aww">');
-		$('body').removeClass('background-cover');
+		// home page slideshow
+		$('.slideshow').html('<img src="http://placekitten.com/g/825/508" alt="aww">');		
+		$('body.home').removeClass('background-cover');
 		
+
+		$('.dropclickmenu:not(.dropclickedmenu)').each(function() {
+		
+			$(this).append('<div class="dropclickmenu-display"></div>');
+			$(this).children('.dropclick').addClass('dropclick-in-menu');
+			$(this).addClass('dropclickedmenu');
+			
+		});
+
 		$('.dropclick:not(.dropclicked)').each(function() {
+		
 			// trick this out into a slick little drop down menu
 			
 			$(this).children().wrapAll('<div class="dropclick-items"></div>');
 			
 			var id = $(this).attr('id');
+			if (!id) { id = "some-menu"; }
 			var label = id.replace("-", " ");
 			label = label.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
 			    return $1.toUpperCase();
 			});
 			$(this).prepend('<div class="dropclick-menu">' + label + '</div>');
 			
-			$(this).children('.dropclick-menu').on('click', function() {
-				$(this).siblings('.dropclick-items').slideToggle();
-			});
+			if ($(this).hasClass('dropclick-in-menu')) {
+				$(this).children('.dropclick-menu').on('click', function() {
+
+					var disp = $(this).parent().siblings('.dropclickmenu-display');
+				
+					if ($(this).parent().hasClass('open')) {
+						$(this).parent().removeClass('open');
+						disp.slideUp('fast').html("&nbsp;");
+					} else {
+						$(this).parent().siblings('.dropclicked').removeClass('open');
+						$(this).parent().addClass('open');
+						var items = $(this).siblings('.dropclick-items').clone();						
+						disp.slideUp('fast', function() {
+							$(this).html(items).slideDown();
+						});
+					}
+					
+				});
+			} else {
+				$(this).children('.dropclick-menu').on('click', function() {
+					$(this).siblings('.dropclick-items').slideToggle();
+				});
+			}
 			
 			$(this).addClass('dropclicked');
 			
@@ -56,11 +88,27 @@ function viewportGo() {
 	
 	} else if (thisviewport == "l") {
 	
+		// home page slideshow
 		$('.slideshow').html('&nbsp;');
-		$('body').addClass('background-cover');
-	
+		$('body.home').addClass('background-cover');
+		
+		undropclick();
+		
 	}
 	
 	viewportSetTitle(viewport, thisviewport);
 
+}
+
+function undropclick() {
+
+	$('.dropclickedmenu').removeClass('dropclickedmenu');
+	$('.dropclickmenu-display').remove();
+	
+	$('.dropclicked').each(function() {
+	
+		$(this).children('.dropclick-menu').remove();
+		$(this).removeClass('dropclicked').removeClass('dropclicked-in-menu');
+	
+	});
 }
